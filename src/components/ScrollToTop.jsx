95a090503583bcda490b9a91c67cb4d548c99294
Jas Shroff <jasshroff1@gmail.com>
2026-08-23 22:34:27 +0530

    feat: integrate Google Analytics 4 with page view tracking for route changes

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,11 +2,17 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
+    const { pathname, search } = useLocation();
 
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, [pathname]);
+
+        if (typeof window.gtag === 'function') {
+            window.gtag('config', 'G-RVWB6TK477', {
+                page_path: pathname + search,
+            });
+        }
+    }, [pathname, search]);
 
     return null;
 };
